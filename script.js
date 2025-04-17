a0647d056cd7458851053804e6a5d534d05d5c5a
RodrigoLima1508 <rodrigofelicianolima@gmail.com>
2025-04-17 14:15:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Adiciona um evento de clique para ampliar as imagens da galeria
-document.querySelectorAll('.gallery img').forEach(image => {
-    image.addEventListener('click', () => {
-        window.open(image.src, '_blank');
-    });
-});
+
